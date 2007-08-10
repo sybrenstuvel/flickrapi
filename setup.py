fe6a90bfda4566e2ab34753e41b20704131ece22
Sybren Stüvel <sybren@stuvel.eu>
2007-08-10 21:42:00 +0000

    Initial checkin of Beej's FlickrAPI

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='FlickrAPI',
+	version='11',
+	url='http://beej.us/flickr/flickrapi/',
+	author='Beej Jorgensen',
+	author_email='beej@beej.us',
+	description='Python interface to the Flickr API',
+	py_modules=['flickrapi'])
