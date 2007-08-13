d29f8a9f43da1f80a063d7c0e67307e3a09864f8
Sybren Stüvel <sybren@stuvel.eu>
2007-08-13 19:30:02 +0000

    Added docstring Replaced tabs with spaces

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+
+'''Python distutils install script.
+
+Run with "python setup.py install" to install FlickrAPI
+'''
+
 from distutils.core import setup
 
-setup(name='FlickrAPI',
-	version='0.11',
-	url='http://beej.us/flickr/flickrapi/',
-	author='Beej Jorgensen',
-	author_email='beej@beej.us',
-	description='Python interface to the Flickr API',
-	py_modules=['flickrapi'])
+from flickrapi import __version__
+
+setup(name='flickrapi', 
+    version=__version__, 
+    url='http://flickrapi.sourceforge.net/', 
+    author='Beej Jorgensen', 
+    author_email='beej@beej.us', 
+    description='Python interface to the Flickr API', 
+    py_modules=['flickrapi'])
