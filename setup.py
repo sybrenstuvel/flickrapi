c5f31bd375fdac7c985823ed1ac9fd44396c1c28
Sybren Stüvel <sybren@stuvel.eu>
2007-08-21 20:10:05 +0000

    Removed dependency on GNU Make when installing, generating documentation in setup.py is now done in pure Python.

--- setup.py
+++ setup.py
@@ -9,7 +9,17 @@ Run with "python setup.py install" to install FlickrAPI
 from distutils.core import setup, Distribution
 import os
 
+try:
+    raise ImportError
+    import docutils.core
+except ImportError:
+    docutils = None
+
 from flickrapi import __version__
+
+# This will be set to True when either the documentation is already
+# there, or if we can build it.
+documentation_available = False
 
 class OurDistribution(Distribution):
     '''Distribution that also generates the documentation HTML'''
@@ -19,25 +29,40 @@ class OurDistribution(Distribution):
         the superclass' run_command(...) method.
         '''
 
-        # TODO: build documentation
-        if command == 'install_data':
-            os.system("make -C doc")
-        if command == 'clean':
-            os.system("make -C doc clean")
+        if command == 'install_data' and docutils:
+            publisher = docutils.core.Publisher()
+            docutils.core.publish_file(writer_name='html',
+                    source=open('doc/documentation.rst'),
+                    source_path='doc',
+                    destination=open('doc/documentation.html', 'w'),
+                    destination_path='doc',
+                    settings_overrides={'stylesheet_path': 'doc/documentation.css'}
+            )
         Distribution.run_command(self, command)
 
-setup(name='flickrapi', 
-    version=__version__, 
-    url='http://flickrapi.sourceforge.net/', 
-    author='Beej Jorgensen', 
-    author_email='beej@beej.us', 
-    maintainer='Sybren Stüvel',
-    maintainer_email='sybren@stuvel.eu',
-    description='Python interface to the Flickr API', 
-    packages=['flickrapi'],
-    data_files=[
-        ('share/doc/flickrapi-%s' % __version__, ['doc/documentation.html', 'doc/documentation.css', 'doc/html4css1.css']),
-    ],
-    license='Python',
-    distclass=OurDistribution
-)
+data = {
+    'name': 'flickrapi', 
+    'version': __version__, 
+    'url': 'http://flickrapi.sourceforge.net/', 
+    'author': 'Beej Jorgensen', 
+    'author_email': 'beej@beej.us', 
+    'maintainer': 'Sybren Stüvel',
+    'maintainer_email': 'sybren@stuvel.eu',
+    'description': 'Python interface to the Flickr API', 
+    'packages': ['flickrapi'],
+    'data_files': [],
+    'license': 'Python',
+    'distclass': OurDistribution
+}
+
+alldocs = ['doc/documentation.html', 'doc/documentation.css', 'doc/html4css1.css']
+
+if docutils or all(os.path.exists(doc) for doc in alldocs):
+    # Only include documentation if it can be built, or if it has been
+    # built already
+    data['data_files'].append(('share/doc/flickrapi-%s' % __version__, alldocs))
+    documentation_available = True
+else:
+    print "WARNING: Unable to import docutils, documentation will not be included"
+
+setup(**data)
