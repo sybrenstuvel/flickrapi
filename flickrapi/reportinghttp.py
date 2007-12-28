bf7039e0967135eb5a41075a69182dd01441d218
Sybren Stüvel <sybren@stuvel.eu>
2007-12-28 12:32:02 +0000

    Added docstrings

--- flickrapi/reportinghttp.py
+++ flickrapi/reportinghttp.py
@@ -15,8 +15,8 @@ LOG = logging.getLogger(__name__)
 progress_callback = None
 
 class ReportingSocket(object):
-    '''Wrapper around a socket. Gives progress report
-    through a callback function.
+    '''Wrapper around a socket. Gives progress report through a
+    callback function.
     '''
     
     min_chunksize = 10240
@@ -25,6 +25,10 @@ class ReportingSocket(object):
         self.socket = socket
 
     def sendall(self, bits):
+        '''Sends all data, calling the callback function for every
+        sent chunk.
+        '''
+
         LOG.debug("SENDING: %s..." % bits[0:30])
         total = len(bits)
         sent = 0
@@ -41,22 +45,36 @@ class ReportingSocket(object):
             bits = bits[chunksize:]
     
     def makefile(self, mode, bufsize):
+        '''Returns a file-like object for the socket.'''
+
         return self.socket.makefile(mode, bufsize)
     
     def close(self):
+        '''Closes the socket.'''
+
         return self.socket.close()
     
 class ProgressHTTPConnection(httplib.HTTPConnection):
+    '''HTTPConnection that gives regular progress reports during
+    sending of data.
+    '''
 
     def connect(self):
+        '''Connects to a HTTP server.'''
+
         httplib.HTTPConnection.connect(self)
         self.sock = ReportingSocket(self.sock)
         
 class ProgressHTTPHandler(urllib2.HTTPHandler):
+    '''HTTPHandler that gives regular progress reports during sending
+    of data.
+    '''
     def http_open(self, req):
         return self.do_open(ProgressHTTPConnection, req)
 
 def set_callback(method):
+    '''Sets the callback function to use for progress reports.'''
+
     global progress_callback # IGNORE:W0603
 
     if not callable(method):
@@ -65,12 +83,16 @@ def set_callback(method):
     progress_callback = method
 
 def urlopen(url_or_request, callback, body=None):
+    '''Opens an URL using the ProgressHTTPHandler.'''
+
     set_callback(callback)
     opener = urllib2.build_opener(ProgressHTTPHandler)
     return opener.open(url_or_request, body)
 
 if __name__ == '__main__':
     def upload(progress, finished):
+        '''Upload progress demo'''
+
         LOG.info("%3.0f - %s" % (progress, finished))
     
     conn = urlopen("http://www.flickr.com/", 'x' * 10245, upload)
