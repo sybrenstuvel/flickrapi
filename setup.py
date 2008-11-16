f501d299faa0f02dbf153c375e2c0692718cfda5
Sybren A. Stüvel <sybren@stuvel.eu>
2008-11-16 20:13:52 +0100

    More pythonic code in version check

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import sys
 # Check the Python version
 (major, minor) = sys.version_info[:2]
 
-if major < 2 or (major == 2 and minor < 4):
+if (major, minor) < (2, 4):
     raise SystemExit("Sorry, Python 2.4 or newer required")
 
 import _setup
