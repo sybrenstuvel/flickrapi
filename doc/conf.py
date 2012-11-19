324343f1f70fe90f3a38e5a676a31e95e7132df3
Sybren A. Stüvel <sybren@stuvel.eu>
2012-11-19 22:55:50 +0100

    Explicit UTF-8 encoding, added viewcode sphinx extension

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.viewcode']
 
 todo_include_todos = True
 
@@ -36,7 +36,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+source_encoding = 'utf-8'
 
 # The master toctree document.
 master_doc = 'index'
