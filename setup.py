e21a79f20345f23e68a3e11029d6916b132e2e5c
Sybren A. Stüvel <sybren@stuvel.eu>
2014-05-16 10:16:23 +0200

    Using setuptools instead of distribute; bumped version to 1.4.3

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@
 Run with "python setup.py install" to install FlickrAPI
 '''
 
-import distribute_setup
-distribute_setup.use_setuptools()
-
 import sys
 
 # Check the Python version
 (major, minor) = sys.version_info[:2]
 
 if (major, minor) < (2, 4):
-    raise SystemExit("Sorry, Python 2.4 or newer required")
+    raise SystemExit("Sorry, Python 2.4 or newer 2.x version required")
+
+if major >= 3:
+    raise SystemExit('Sorry, Python 3.x is not supported. Support will be '
+    'introduced in version 2.0; for now try the version from our Mercurial '
+    'repository at https://bitbucket.org/sybren/flickrapi or use Python 2.7.')
 
 import _setup
