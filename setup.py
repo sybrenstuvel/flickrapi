d517e45a8703014bdd738c3be6290102cf181fe8
Thijs Triemstra <info@collab.nl>
2017-05-07 23:15:32 +0200

    add python 3.6 classifier

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ data = {
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Multimedia :: Graphics',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
