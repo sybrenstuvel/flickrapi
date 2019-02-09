8a0ea407c54ff958af0be0a97eaba08d4fee8baf
Hugo <hugovk@users.noreply.github.com>
2019-02-09 12:56:23 +0200

    Add support for Python 3.7

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ data = {
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Multimedia :: Graphics',
